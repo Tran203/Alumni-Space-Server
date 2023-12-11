1338da0fa85f6999defb06cf885202a7b15a388e
Tran203 <tshetshe203@gmail.com>
2023-12-11 17:23:09 +0200

    update

--- database/database.js
+++ database/database.js
@@ -22,12 +22,20 @@ module.exports = {
 
 var mysql = require('mysql');
 
+// const client = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     port:"3306",
+//     database: 'test',
+//   });
+
 const client = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    port:"3306",
-    database: 'test',
-  });
+  host: 'sql8.freemysqlhosting.net',
+  user: 'sql8669541',
+  password: 'EdvBgRv8sW',
+  port:"3306",
+  database: 'sql8669541',
+});
 
   client.on('connection', (connection) => {
     console.log('Connected to database');
