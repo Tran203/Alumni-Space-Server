1ba7b4a51ff2f559774ac78e4ae3bd0e71c601ba
Tran203 <tshetshe203@gmail.com>
2024-01-23 19:57:32 +0200

    Update my-network.js

--- routes/my-network.js
+++ routes/my-network.js
@@ -1,4 +1,4 @@
-const app = require('express')();
+/*const app = require('express')();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer, {
   cors: { origin: '*' }
@@ -217,4 +217,4 @@ io.on('connection', (socket) => {
 
 module.exports = app;
 
-httpServer.listen(port, () => console.log(`listening on port ${port}`));
+httpServer.listen(port, () => console.log(`listening on port ${port}`));*/
